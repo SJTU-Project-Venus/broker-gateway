802544d39f0da0a5c90ee9e6b52878393c9d50fe
macoredroid <coredroid0401@gmail.com>
2020-06-07 20:55:01 +0800

    first commit

--- src/main/java/macoredroid/brokergateway/controller/FutureController.java
+++ src/main/java/macoredroid/brokergateway/controller/FutureController.java
@@ -1,15 +1,15 @@
 package macoredroid.brokergateway.controller;
 
-import macoredroid.brokergateway.Entity.FutureEntity;
 import macoredroid.brokergateway.DTO.FutureDTO;
+import macoredroid.brokergateway.Entity.FutureEntity;
+import macoredroid.brokergateway.Entity.MarketDepthEntity;
 import macoredroid.brokergateway.command.NewFutureCommand;
+import macoredroid.brokergateway.model.Response;
 import macoredroid.brokergateway.repository.FutureRepository;
+import macoredroid.brokergateway.repository.MarketDepthRepository;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,7 +20,9 @@ public class FutureController {
     CommandGateway commandGateway;
     @Autowired
     FutureRepository futureRepository;
-    @PostMapping
+    @Autowired
+    MarketDepthRepository marketDepthRepository;
+    @PostMapping("/addFuture")
     public void addFuture(@RequestBody FutureDTO futureDTO){
         //no duplicate
         if(futureRepository.findFutureDTOByNameEquals(futureDTO.getName())!=null)
@@ -31,5 +33,15 @@ public class FutureController {
         future.setName(futureDTO.getName());
         future.setMarketDepthId(UUID.randomUUID().toString());
         commandGateway.send(new NewFutureCommand(UUID.randomUUID().toString(), future));
+    }
+
+    @GetMapping("/getMarketDepth/{futureName}")
+    public Response<MarketDepthEntity> getMarketDepth(@PathVariable String futureName){
+        if(futureRepository.findFutureDTOByNameEquals(futureName)==null)
+        {
+            return new Response<>(null, 500, "no such future");
+        }
+        String id=futureRepository.findFutureDTOByNameEquals(futureName).getMarketDepthId();
+        return new Response<MarketDepthEntity>(marketDepthRepository.findById(id).get(), 200, "ok");
     }
 }
