098e474faa9f03a9f288cc76e24387aeec92695a
macoredroid <coredroid0401@gmail.com>
2020-06-08 21:30:16 +0800

    update

--- src/main/java/macoredroid/brokergateway/DTO/CancelOrderDTO.java
+++ src/main/java/macoredroid/brokergateway/DTO/CancelOrderDTO.java
@@ -1,11 +1,13 @@
 package macoredroid.brokergateway.DTO;
 
 import lombok.Data;
+import macoredroid.brokergateway.model.Type;
+
 @Data
 public class CancelOrderDTO {
 
     String targetId;
-
+    Type targetType;
     private String futureName;
 
 
